71538fe4e4a552bbddb41711df448d418c486e11
ccnarvaez <ccnarvaez90@gmail.com>
2021-02-13 18:19:22 -0300

    Cambios 13022021- Maskify-IsValid-Ok

--- src/index.js
+++ src/index.js
@@ -2,39 +2,43 @@
  // Validacion, importacion desde el modulo  
  import validator from './validator.js';
 
- console.log('ENTRO');
- console.log(validator.maskify(1234567891234567));
- 
-//0. Dinamismo pagina de inicio- pagina validacion
+
+//Dinamismo pagina de inicio- pagina validacion
  let buttonClickHomePage=document.getElementById('buttonHomePage');
- buttonClickHomePage.addEventListener('click', move);
+ buttonClickHomePage.addEventListener('click', move1);
  
- function move(){
+ function move1(){
      document.getElementById('validation').style.display='block';
-     document.getElementById('buttonHomePage').style.display='none';
+     document.getElementById('buttonHomePage').style.display='none';     
  }
 
-
-//a. Almacenamiento de datos en variables
+//Almacenamiento de datos en variables
 
 let buttonClick = document.getElementById('button');
 buttonClick.addEventListener('click', savingNumber);
 
 function savingNumber (){
 
-    let creditCardNumber = document.getElementById('number').value;
+    let creditCardNumber = document.getElementById('number').value; // Se guarda numero de tarjeta
+    document.getElementById('number').value=validator.maskify(creditCardNumber);// Se sustituyen valores por '#' luego de que el usuario ingrese numero
+    
+    // Comprobaciones (eliminar al final)
     console.log(creditCardNumber);
-
+    console.log(validator.maskify(creditCardNumber));
+    console.log(validator.isValid(creditCardNumber));
+    
     //b. Mensajes de error por numeros no validos
     while (creditCardNumber ==0||creditCardNumber ==null||creditCardNumber.length<16) {
-    alert("Por favor ingresa numero valido");
-    break;
+        alert("Por favor ingresa numero valido");
+        break;
     } 
-
+    
+   
+    
 }
 
 
+          
 
-//d. ocultar ultimos 4 digitos
 
-// e. mensaje validacion ok u validacion erronea
+ 
--- src/validator.js
+++ src/validator.js
@@ -1,24 +1,60 @@
 const validator = {
 
-  // Declaro funcion para enmascarar considerando que estoy dentro de un objeto
+  //1. Validacion tarjeta de credito
+
+  isValid:(creditCardNumber)=>{
+
+    let creditCardStep1= 0;
+    let finalAdittion=0;
+    
+    for (let i=0; i<=creditCardNumber.length-1; i++){
+
+        // tratamiento numeros pares
+        if (i%2==0){
+          creditCardStep1+=creditCardNumber[i]*2
+        }
+
+        // tratamiento impares
+
+        else{
+          creditCardStep1+=creditCardNumber[i];
+        }
+    }
+
+  //Hacer suma global de los digitos internos
+
+    for (let j=0; j<=creditCardStep1.length-1; j++){
+
+        finalAdittion+=parseInt(creditCardStep1.charAt(j));
+    }
+
+  //Condicion de validez o invalidez
+
+   finalAdittion%10==0?alert('Tarjeta valida'):alert('Tarjeta no valida');
+
+  return finalAdittion;
+  },
+
+
+  // 2. Maskify
   maskify: (creditCardNumber)=>{
 
+  //Variable para contener nuevo numero de tarjeta enmascarado
   let cardMaskify='';
-   
-
-
+  
   //Se desea sustituir el valor de los digitos de la tarjeta por '#', a excepcion de los ultimos 4
   
-  for (let i=0; i< creditCardNumber.length-4; i++){
+  for (let i=0; i< creditCardNumber.length; i++){
 
     // Si la posicion i es menor a la posicion menos los ultimos 4, entonces se debe sustituir el valor por #
       if (i<creditCardNumber.length-4){
-        cardMaskify[i]='#';
+        cardMaskify+='#';
+        
       }
 
     // Cuando se trata de los ultimos numeros, se debe igualar al valor almacenado en creditCardNumber, en la posicion indicada
       else{
-        cardMaskify[i]=creditCardNumber[i];
+        cardMaskify+=creditCardNumber[i];
       }
       
   }
